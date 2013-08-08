dbf87fe0d208e723c3fa46ed6778a7f7172f9015
Julian Montez <julian.montez@gmail.com>
2013-08-07 20:22:33 -0500

    Clarified variable names.

--- files/build_srv.py
+++ files/build_srv.py
@@ -28,19 +28,18 @@ pyrax.set_setting('identity_type', 'rackspace')
 pyrax.keyring_auth(args['--username'])
 cs = pyrax.connect_to_cloudservers(region=args['--region'])
 
-key = open(args['--public_key'])
-public_key = {'/root/.ssh/authorized_keys': key}
+public_key = {'/root/.ssh/authorized_keys': open(args['--public_key'])}
 
 image = next(img for img in cs.images.list() if args['--image'] in img.name)
 flavor = next(flv for flv in cs.flavors.list() if args['--flavor'] in flv.name)
 
-req = cs.servers.create(args['<name>'], image.id, flavor.id,
+request = cs.servers.create(args['<name>'], image.id, flavor.id,
   files = public_key)
-build = pyrax.utils.wait_until(req, "status", ["ACTIVE", "ERROR"], attempts = 0,
-  interval = 30, verbose = False)
-srv = cs.servers.get(build.id)
+build = pyrax.utils.wait_until(request, "status", ["ACTIVE", "ERROR"], attempts = 0,
+  interval = 30, verbose = True)
+server = cs.servers.get(build.id)
 
-pub_ipv4 = next(addr for addr in srv.networks['public'] if '.' in addr)
+public_ipv4 = next(addr for addr in server.networks['public'] if '.' in addr)
 
 print "--"
-print "Public IPv4 address: ", pub_ipv4
+print "Public IPv4 address: ", public_ipv4
