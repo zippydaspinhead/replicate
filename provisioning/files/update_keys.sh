e09f6661cf96319c314069b7d7dcd35284be950e
Julian Montez <julian.montez@gmail.com>
2013-08-22 15:39:27 -0500

    Update update_keys.sh

--- provisioning/files/update_keys.sh
+++ provisioning/files/update_keys.sh
@@ -4,7 +4,7 @@ rm -rf /root/.ssh/
 mkdir -p /root/.ssh/
 
 git clone https://github.com/jpmontez/replicate.git
-mv /root/replicate/files/public_keys /root/.ssh/authorized_keys
+mv /root/replicate/provisioning/files/public_keys /root/.ssh/authorized_keys
 chown root:root /root/.ssh/authorized_keys
 chown root:root /root/.ssh/
 chmod 600 /root/.ssh/authorized_keys
