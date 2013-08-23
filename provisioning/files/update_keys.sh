65f1f6ad139fed5b718026b62c1aaa368c3d1c04
Julian Montez <julian.montez@gmail.com>
2013-08-22 21:13:01 -0500

    Modified script to preserve permissions.

--- provisioning/files/update_keys.sh
+++ provisioning/files/update_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl --remote-name https://raw.github.com/jpmontez/replicate/master/provisioning/files/public_keys 
-rsync --checksum --perms --no-o --no-g /root/public_keys /root/.ssh/authorized_keys
+rsync /root/public_keys /root/.ssh/authorized_keys
 rm -rf /root/public_keys
