1f6beb4a441386eb526902f19273276d669b69b2
Julian Montez <julian.montez@gmail.com>
2013-08-22 20:10:13 -0500

    Implement rsync to bash script.

--- provisioning/files/update_keys.sh
+++ provisioning/files/update_keys.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-rm -rf /root/replicate/
-rm -rf /root/.ssh/
-mkdir -p /root/.ssh/
-
-git clone https://github.com/jpmontez/replicate.git
-mv /root/replicate/provisioning/files/public_keys /root/.ssh/authorized_keys
-chown root:root /root/.ssh/authorized_keys
-chown root:root /root/.ssh/
-chmod 600 /root/.ssh/authorized_keys
-chmod 700 /root/.ssh/
+curl --remote-name https://raw.github.com/jpmontez/replicate/master/provisioning/files/public_keys 
+rsync --checksum --perms --no-o --no-g /root/public_keys /root/.ssh/authorized_keys
+rm -rf /root/public_keys
